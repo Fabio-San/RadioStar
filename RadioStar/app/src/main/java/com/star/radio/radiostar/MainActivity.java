5bb018da293b8670a2b62ef458e52f8895bf7426
Fabio <fabietto089@gmail.com>
2016-05-06 00:45:10 +0200

    Main Activity: changements
    - Bug fixes
    - New notification method
    - New messages
    - Ui Optimization

--- RadioStar/app/src/main/java/com/star/radio/radiostar/MainActivity.java
+++ RadioStar/app/src/main/java/com/star/radio/radiostar/MainActivity.java
@@ -12,6 +12,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,13 +30,8 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     boolean isPlaying=false;
+
     private final static String RADIO_STATION_URL = "http://91.194.90.147:8010/;stream.mp3";
-
-    //private ProgressBar playSeekBar;
-
-    //private ImageButton buttonPlay;
-
-    //private ImageButton buttonStopPlay;
 
     private MediaPlayer player;
 
@@ -54,17 +50,6 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private void initializeUIElements() {
 
-        //playSeekBar = (ProgressBar) findViewById(R.id.progressBar1);
-        //playSeekBar.setMax(100);
-        //playSeekBar.setVisibility(View.INVISIBLE);
-
-        //buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
-        //buttonPlay.setOnClickListener(this);
-
-        //buttonStopPlay = (ImageButton) findViewById(R.id.buttonStopPlay);
-        //buttonStopPlay.setEnabled(false);
-        //buttonStopPlay.setOnClickListener(this);
-
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean initialDialogDisplayed = preferences.getBoolean("InitialDialog", false);
         if (!initialDialogDisplayed) {
@@ -74,11 +59,11 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
             // Display the dialog here
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle(" Attenzione");
-            builder.setMessage(" Benvenuti nell'App Radio Star! Questa app necessita di una connessione ad internet per riprodurre la diretta basata sul sito web di www.radiostarnews.it."
-                    + " Abilitate quindi una rete WiFi o una rete dati prima di premere il tasto Play."
-                    + "Grazie per l'attenzione!")
-                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+            builder.setTitle("Attenzione");
+            builder.setMessage("Benvenuti nell'App Radio Star! Questa app necessita di una connessione ad internet per riprodurre la diretta basata sul sito web di www.radiostarnews.it."
+                    + "\nAbilitate quindi una rete WiFi o una rete dati prima di premere il tasto Play."
+                    + "\nGrazie per l'attenzione!")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
 
                         public void onClick(DialogInterface dialog, int id) {
                         }
@@ -97,11 +82,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     public void onClick(View v) {
         //sul click del FAB
         if(!isOnline()){
-            Context context = getApplicationContext();
-            CharSequence text = "Non sei Connesso!";
-            int duration = android.widget.Toast.LENGTH_LONG;
-            android.widget.Toast toast = android.widget.Toast.makeText(context, text, duration);
-            toast.show();
+            Snackbar.make(findViewById(R.id.coordinatorLayout), "Connessione assente", Snackbar.LENGTH_SHORT).show();
         }
             if(isPlaying){
             if(player.isPlaying()) {
@@ -128,11 +109,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             isPlaying=true;
             player.setLooping(true);
             player.isLooping();
-                Context context = getApplicationContext();
-                CharSequence text = "In Connessione..";
-                int duration = android.widget.Toast.LENGTH_LONG;
-                android.widget.Toast toast = android.widget.Toast.makeText(context, text, duration);
-                toast.show();
+                Snackbar.make(findViewById(R.id.coordinatorLayout), "Connesso", Snackbar.LENGTH_SHORT).show();
                 Intent intent = new Intent(this, MainActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
                 NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
@@ -143,69 +120,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 notificationManager.notify(0, mBuilder.build());
             }
-            //startPlaying();
-        //Context context = getApplicationContext();
-        //CharSequence text = "In Connessione...";
-        //int duration = android.widget.Toast.LENGTH_LONG;
-        //android.widget.Toast toast = android.widget.Toast.makeText(context, text, duration);
-        //toast.show();
-        //Object nm;
-        //nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
-        //NotificationCompat.Builder mBuilder =
-
-        //(NotificationCompat.Builder) new NotificationCompat.Builder(this)
-        //.setSmallIcon(R.drawable.ic_launcher)
-        //.setContentTitle("Radio Star")
-        //.setContentText("In Diretta");
-        //PendingIntent.getActivity(this, 0, new Intent(), PendingIntent.FLAG_UPDATE_CURRENT);
-        //NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        //notificationManager.notify( 0, mBuilder.build());
-        //} else if (v == buttonStopPlay) {
-        //stopPlaying();
-        //Intent svc=new Intent(this, BackgroundSoundService.class);
-        //startService(svc);
-                 }
-            }
-    //}
-
-    //private void startPlaying() {
-        //if (player.isPlaying()) {
-//buttonPlay.setVisibility(View.INVISIBLE);
-//}
-//buttonStopPlay.setEnabled(true);
-                //buttonPlay.setEnabled(false);
-//buttonPlay.setVisibility(View.INVISIBLE);
-//playSeekBar.setVisibility(View.VISIBLE);
-
-
-//player.prepareAsync();
-
-    //player.setOnPreparedListener(new OnPreparedListener() {
-
-            //public void onPrepared(MediaPlayer mp) {
-    //player.start();
-            //}
-
-        //});
-
-    //}
-
-//private void stopPlaying() {
-//if (player.isPlaying()) {
-    //player.stop();
-//player.release();
-//initializeMediaPlayer();
-//NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
-//notificationManager.cancel(0);
-//}
-
-    //buttonPlay.setEnabled(true);
-    //buttonStopPlay.setEnabled(false);
-    //playSeekBar.setVisibility(View.INVISIBLE);
-    //buttonPlay.setVisibility(View.VISIBLE);
-
-    //}
+                }
+    }
 
     private void initializeMediaPlayer() {
         player = new MediaPlayer();
@@ -219,15 +136,6 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             e.printStackTrace();
         }
     }
-
-        //player.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
-
-            //public void onBufferingUpdate(MediaPlayer mp, int percent) {
-                //playSeekBar.setSecondaryProgress(percent);
-                //Log.i("Buffering", "" + percent);
-            //}
-        //});
-    //}
 
     @Override
     public void onBackPressed()
